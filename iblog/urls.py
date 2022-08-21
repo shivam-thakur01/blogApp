cad82c25d33d489918bb15b88d55e2f257868029
Shivam Thakur <shivam.thakur.ug20@nsut.ac.in>
2022-08-21 16:41:26 +0530

    change url

--- iblog/urls.py
+++ iblog/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings 
+from blogs.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogs.urls')),
+    path('',home),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
